dd3795c7b169ef77dfe386fc51523ff976631fc6
Ben Simmonds <ben@bencode.net>
2023-06-25 19:51:46 +1000

    shuffling

---
+++ effective/07_enumerate_over_range.py
@@ -0,0 +1,23 @@
+from random import randint
+
+random_bits = 0
+
+# range is good fit for simple numeric iteration
+for i in range(32):
+    if randint(0, 1):
+        random_bits |= 1 << i
+print(bin(random_bits))
+
+# 'in' for data structure iteration
+flavor_list = ['vanilla', 'chocolate', 'pecan', 'strawberry']
+for flavor in flavor_list:
+    print(f'{flavor} is delicious')
+
+# stick with enumerate which yields both index and item
+it = enumerate(flavor_list)
+print(next(it))
+print(next(it))
+
+# notice the tuple results, these supports unpacking (destructuring)
+for i, flavor in enumerate(flavor_list):
+    print(f'{i + 1}: {flavor}')
